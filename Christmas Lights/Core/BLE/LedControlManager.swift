0efc291c11c87dc1251e7c715535cb57349ab7ee
Mikhail Muravev <mixaillanatolich@gmail.com>
2020-11-13 11:17:01 +0300

    Update leds control commands

--- Christmas Lights/Core/BLE/LedControlManager.swift
+++ Christmas Lights/Core/BLE/LedControlManager.swift
@@ -11,9 +11,19 @@ import UIKit
 
 let LEDController = LedControlManager.sharedInstance
 
-class LedControlManager: NSObject {
+enum LedSolidColors: UInt32 {
+    case black = 0x20
+    case red = 0x21
+    case orange = 0x22
+    case yellow = 0x23
+    case green = 0x24
+    case skyblue = 0x25
+    case blue = 0x26
+    case violet = 0x27
+    case white = 0x28
+}
 
-  //  fileprivate let divider: UInt8 = 0x2c
+class LedControlManager: NSObject {
     
     public static let sharedInstance: LedControlManager = {
         let instance = LedControlManager()
@@ -25,75 +35,157 @@ class LedControlManager: NSObject {
     }
     
     func sendPing(callback: @escaping (_ isSuccess: Bool) -> Void) {
-        
         makeCommandAndSend(data: Data([0x00]),
                            isWaitResponse: true, isWriteWithResponse: true,
                            command: GyverStateCommand.self) { (status, response) in
-                            
                             callback(status && response != nil)
         }
-        
     }
     
     func loadSetting(callback: @escaping (_ isSuccessful: Bool, _ response: GyverStateResponse?) -> Void) {
-        
-        
         makeCommandAndSend(data: Data([0x01]),
                            isWaitResponse: true, isWriteWithResponse: true,
                            command: GyverStateCommand.self) { (status, response) in
-                            
                             callback(status, response as? GyverStateResponse)
         }
-
     }
     
-    func changeSetting(command: Data, callback: @escaping (_ isSuccess: Bool) -> Void) {
-        
-        makeCommandAndSend(data: command,
-                           isWaitResponse: false, isWriteWithResponse: false,
-                           command: BLECommand.self) { (status, response) in
-                            
+    func setMode(_ id: Int, callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        let commandId:UInt8 = 0x03
+        let modeId:UInt8 = UInt8(id)
+        makeCommandAndSend(data: Data([commandId, modeId]),
+                           isWaitResponse: true, isWriteWithResponse: true,
+                           command: SetModeCommand.self) { (status, response) in
                             callback(status)
         }
-        
     }
     
-    func changePreset(presetId id: Int, callback: @escaping (_ isSuccessful: Bool, _ response: GyverStateResponse?) -> Void) {
+    //MARK: - brightness
+    func brightnessPlusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x01, callback: callback)
+    }
+    
+    func brightnessMinusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x02, callback: callback)
+    }
+    
+    //MARK: -
+    func resetCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x03, callback: callback)
+    }
+    
+    func randomCmd(random: Bool, callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(random ? 0x05 : 0x04, callback: callback)
+    }
+    
+    func stopCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x19, callback: callback)
+    }
+    
+    //MARK: - led count
+    func ledPlusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x08, callback: callback)
+    }
+    
+    func ledMinusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x09, callback: callback)
+    }
+    
+    //MARK: -
+    func changeEffectDirectionCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0a, callback: callback)
+    }
+    
+    func speedMinusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0b, callback: callback)
+    }
+    
+    func speedPlusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0c, callback: callback)
+    }
+    
+    //MARK: -
+    func glitterCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0d, callback: callback)
+    }
+    
+    func backgroudCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0e, callback: callback)
+    }
+    
+    func candleCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x0f, callback: callback)
+    }
+    
+    //MARK: -
+    func prevModeCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x10, callback: callback)
+    }
+    
+    func nextModeCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x11, callback: callback)
+    }
+    
+    //MARK: -
+    func delayMinusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x13, callback: callback)
+    }
+    
+    func delayPlusCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x14, callback: callback)
+    }
+    
+    //MARK: - Palette
+    func paletteStopCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x15, callback: callback)
+    }
+    
+    func palettePreviousCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x16, callback: callback)
+    }
+    
+    func paletteNextCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x17, callback: callback)
+    }
+    
+    func paletteAutoCmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(0x18, callback: callback)
+    }
+    
+    //MARK: - colors
+    func setColorCmd(_ color: LedSolidColors, callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        sendCommand(color.rawValue, callback: callback)
+    }
+    
+//    //MARK: -
+//    func Cmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+//        sendCommand(0x, callback: callback)
+//    }
+//
+//    func Cmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+//        sendCommand(0x, callback: callback)
+//    }
+//
+//    func Cmd(callback: @escaping (_ isSuccessful: Bool) -> Void) {
+//        sendCommand(0x, callback: callback)
+//    }
+    
+    fileprivate func sendCommand(_ command: UInt32, callback: @escaping (_ isSuccessful: Bool) -> Void) {
+        let commandId:UInt8 = 0x02
         
-        let commandId:UInt8 = 0x03
-        let presetId:UInt8 = UInt8(id)
+        var commandData = Data()
+        commandData.append(Data([commandId]))
+        commandData.append(Data([ UInt8(command & 0x000000FF) ]))
+//        commandData.append(Data([
+//            UInt8(command & 0x000000FF),
+//            UInt8((command & 0x0000FF00) >> 8),
+//            UInt8((command & 0x00FF0000) >> 16),
+//            UInt8((command & 0xFF000000) >> 24)
+//            ]))
         
-        makeCommandAndSend(data: Data([commandId, presetId]),
+        makeCommandAndSend(data: commandData,
                            isWaitResponse: true, isWriteWithResponse: true,
-                           command: GyverStateCommand.self) { (status, response) in
-                            
-                            callback(status, response as? GyverStateResponse)
-        }
-        
-    }
-    
-    func changeMode(modeId id: Int, callback: @escaping (_ isSuccessful: Bool, _ response: GyverStateResponse?) -> Void) {
-        
-        let commandId:UInt8 = 0x04
-        let mode:UInt8 = UInt8(id)
-        
-        makeCommandAndSend(data: Data([commandId, mode]),
-                           isWaitResponse: true, isWriteWithResponse: true,
-                           command: GyverStateCommand.self) { (status, response) in
-                            
-                            callback(status, response as? GyverStateResponse)
-        }
-    }
-    
-    func changeLeds(state isOn: Bool, callback: @escaping (_ isSuccess: Bool) -> Void) {
-        
-        let commandId:UInt8 = 0x05
-        let state:UInt8 = isOn ? 0x01 : 0x00
-        
-        makeCommandAndSend(data: Data([commandId, state]),
-                           isWaitResponse: false, isWriteWithResponse: false,
-                           command: BLECommand.self) { (status, response) in
-                            
+                           command: SetModeCommand.self) { (status, response) in
                             callback(status)
         }
     }
@@ -102,11 +194,12 @@ class LedControlManager: NSObject {
                                                         isWriteWithResponse: Bool, command: T.Type,
                                                         callback: @escaping (_ isSuccessful: Bool, _ response: BLEResponse?) -> Void) {
         
-        let request = GyverRequest(rawData: data, requestCharacteristic: "FFE1", responseCharacteristic: "FFE1")
+        let request = CommonRequest(rawData: data, requestCharacteristic: "FFE1", responseCharacteristic: "FFE1")
+        request.timeout = 2
         request.isWaitResponse = isWaitResponse
         request.isWriteWithResponse = isWriteWithResponse
         request.mode = .write
-        request.retryCount = 0
+        request.retryCount = 1
         
         let command = command.init(with: request) as! BLECommand
         command.responseCallback = { (status, response, error) in
